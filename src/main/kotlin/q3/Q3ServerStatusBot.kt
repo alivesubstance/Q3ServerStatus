f2aefc21ccf9d304b81ca9453bf5a158c56aa347
mirian <mirian@pleeco.com>
2019-05-22 16:59:31 +0300

    add com.github.pengrad:java-telegram-bot-api:4.2.0

--- src/main/kotlin/q3/Q3ServerStatusBot.kt
+++ src/main/kotlin/q3/Q3ServerStatusBot.kt
@@ -1,16 +1,28 @@
 package q3
 
-import org.telegram.telegrambots.api.objects.Update
-import org.telegram.telegrambots.bots.TelegramLongPollingBot
+import com.pengrad.telegrambot.TelegramBot
 
-object Q3ServerStatusBot: TelegramLongPollingBot() {
 
-    override fun getBotToken() = "735603182:AAF_WMAzBOa1vLxELNDt4EilNE_sHF3OG_4"
-
-    override fun getBotUsername() = "q3serverstatus_bot"
-
-    override fun onUpdateReceived(update: Update?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+fun main(args: Array<String>) {
+    val telegramBot = TelegramBot.Builder("735603182:AAF_WMAzBOa1vLxELNDt4EilNE_sHF3OG_4")
+        .okHttpClient()
 
 }
+
+
+//import org.telegram.telegrambots.api.objects.Update
+//import org.telegram.telegrambots.bots.TelegramLongPollingBot
+//
+//object Q3ServerStatusBot: TelegramLongPollingBot() {
+//
+//    override fun getBotToken() = "735603182:AAF_WMAzBOa1vLxELNDt4EilNE_sHF3OG_4"
+//
+//    override fun getBotUsername() = "q3serverstatus_bot"
+//
+//    override fun onUpdateReceived(update: Update?) {
+//        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+//    }
+//
+//
+//
+//}

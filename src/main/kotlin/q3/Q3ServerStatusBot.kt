5e08ea910ce742a46ee9c02aae01d1835f004a8d
mirian <mirian@pleeco.com>
2019-05-22 17:29:54 +0300

    add log4j default config and dependencies

--- src/main/kotlin/q3/Q3ServerStatusBot.kt
+++ src/main/kotlin/q3/Q3ServerStatusBot.kt
@@ -1,28 +1,54 @@
 package q3
 
 import com.pengrad.telegrambot.TelegramBot
+import okhttp3.OkHttpClient
+import org.telegram.telegrambots.ApiContextInitializer
 
 
-fun main(args: Array<String>) {
-    val telegramBot = TelegramBot.Builder("735603182:AAF_WMAzBOa1vLxELNDt4EilNE_sHF3OG_4")
-        .okHttpClient()
+//fun main(args: Array<String>) {
+//    val telegramBot = TelegramBot.Builder("735603182:AAF_WMAzBOa1vLxELNDt4EilNE_sHF3OG_4")
+//        .okHttpClient(createOkHttpClient())
+//        .build()
+//
+//    telegramBot.
+//}
+//
+//private fun createOkHttpClient(): OkHttpClient {
+//    //TODO configure timeouts
+//    return OkHttpClient()
+//}
+
+
+import org.telegram.telegrambots.api.objects.Update
+import org.telegram.telegrambots.bots.TelegramLongPollingBot
+import org.telegram.telegrambots.TelegramBotsApi
+import org.telegram.telegrambots.exceptions.TelegramApiException
+
+
+
+
+
+object Q3ServerStatusBot: TelegramLongPollingBot() {
+
+    override fun getBotToken() = "735603182:AAF_WMAzBOa1vLxELNDt4EilNE_sHF3OG_4"
+
+    override fun getBotUsername() = "q3serverstatus_bot"
+
+    override fun onUpdateReceived(update: Update?) {
+
+    }
 
 }
 
 
-//import org.telegram.telegrambots.api.objects.Update
-//import org.telegram.telegrambots.bots.TelegramLongPollingBot
-//
-//object Q3ServerStatusBot: TelegramLongPollingBot() {
-//
-//    override fun getBotToken() = "735603182:AAF_WMAzBOa1vLxELNDt4EilNE_sHF3OG_4"
-//
-//    override fun getBotUsername() = "q3serverstatus_bot"
-//
-//    override fun onUpdateReceived(update: Update?) {
-//        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-//    }
-//
-//
-//
-//}
+fun main(args: Array<String>) {
+
+    ApiContextInitializer.init();
+    val botsApi = TelegramBotsApi()
+    try {
+        botsApi.registerBot(Q3ServerStatusBot)
+    } catch (e: TelegramApiException) {
+        e.printStackTrace()
+    }
+
+}

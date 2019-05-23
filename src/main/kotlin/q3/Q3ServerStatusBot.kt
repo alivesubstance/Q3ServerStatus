76f33ff9fdb8a7bd91602426483514d04e241b76
mirian <mirian@pleeco.com>
2019-05-23 16:33:58 +0300

    add log

--- src/main/kotlin/q3/Q3ServerStatusBot.kt
+++ src/main/kotlin/q3/Q3ServerStatusBot.kt
@@ -1,5 +1,6 @@
 package q3
 
+import com.google.common.base.Throwables
 import org.telegram.telegrambots.ApiContextInitializer
 
 import org.telegram.telegrambots.api.objects.Update
@@ -7,13 +8,15 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot
 import org.telegram.telegrambots.TelegramBotsApi
 import org.telegram.telegrambots.exceptions.TelegramApiException
 import com.oracle.util.Checksums.update
+import org.slf4j.LoggerFactory
 import org.telegram.telegrambots.api.methods.send.SendMessage
 import org.telegram.telegrambots.bots.DefaultBotOptions
+import java.lang.RuntimeException
 
 
 object Q3ServerStatusBot: TelegramLongPollingBot(createBotOptions()) {
 
-
+    val log = LoggerFactory.getLogger(Q3ServerStatusBot::class.java)!!
 
     override fun getBotToken() = "735603182:AAF_WMAzBOa1vLxELNDt4EilNE_sHF3OG_4"
 
@@ -25,12 +28,13 @@ object Q3ServerStatusBot: TelegramLongPollingBot(createBotOptions()) {
             val message = SendMessage() // Create a SendMessage object with mandatory fields
                 .setChatId(update.message.chatId)
                 .setText(update.message.text)
+
             try {
                 execute(message) // Call method to send the message
             } catch (e: TelegramApiException) {
-                e.printStackTrace()
+                log.error("Failed to send message", e)
+                Throwables.propagate(e)
             }
-
         }
     }
 
@@ -40,17 +44,4 @@ fun createBotOptions(): DefaultBotOptions {
     val options = DefaultBotOptions()
     options.baseUrl = "http://idi-na-xyz.ru"
     return options
-}
-
-
-fun main(args: Array<String>) {
-    ApiContextInitializer.init();
-    val botsApi = TelegramBotsApi()
-
-    try {
-        botsApi.registerBot(Q3ServerStatusBot)
-    } catch (e: TelegramApiException) {
-        e.printStackTrace()
-    }
-
 }
